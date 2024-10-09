66bca7696abde54073e854d39ae3c0b5163cadf2
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-09 17:31:40 +0300

    Update Program.cs

--- homework05/Program.cs
+++ homework05/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //это тест чтобы проверить сделан ли push 123 git eto kruto
-            //push 2
+            //push 2 <- sent from visual studio
+            //pull 2 < - sent from github
         }
     }
 }
