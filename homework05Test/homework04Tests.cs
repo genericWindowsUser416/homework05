b3dfc5af78c250bab1fe73609092e5a124b63135
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-12 12:03:08 +0300

    made proper spacing in homework04Tests

--- homework05Test/homework04Tests.cs
+++ homework05Test/homework04Tests.cs
@@ -4,7 +4,7 @@ namespace homework05Test
 {
     public class homework04Tests
     {
-        [TestCase("22","11",11)]
+        [TestCase("22", "11", 11)]
         [TestCase("40", "16", 8)]
         public void getBiggestDividerWithEuclidTest(string userInputNumberOne, string userInputNumberTwo, double expected)
         {
