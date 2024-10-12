00cce8d664d7c45293260c81727d4639f0d1cc4f
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-12 10:47:07 +0300

    added getAinpowerBTest

---
+++ homework05Test/homework03Tests.cs
@@ -0,0 +1,20 @@
+﻿using homework05;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework05Test
+{
+    public class homework03Tests
+    {
+        [TestCase("50", "2", 2500)]
+        [TestCase("10", "3", 1000)]
+        public void getAinpowerBTest(string userInputNumberOne, string userInputNumberTwo, double expected)
+        {
+            double actual = homework03.getAinpowerB(userInputNumberOne, userInputNumberTwo);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
