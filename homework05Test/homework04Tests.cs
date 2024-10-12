b464b61cbb20f2299d1a817b62ee387fccaaf50f
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-12 12:02:28 +0300

    added getBiggestDividerWithEuclidTest

--- homework05Test/homework04Tests.cs
+++ homework05Test/homework04Tests.cs
@@ -4,6 +4,12 @@ namespace homework05Test
 {
     public class homework04Tests
     {
-
+        [TestCase("22","11",11)]
+        [TestCase("40", "16", 8)]
+        public void getBiggestDividerWithEuclidTest(string userInputNumberOne, string userInputNumberTwo, double expected)
+        {
+            double actual = homework04.getBiggestDividerWithEuclid(userInputNumberOne, userInputNumberTwo);
+            Assert.That(actual, Is.EqualTo(expected));
+        }
     }
 }
