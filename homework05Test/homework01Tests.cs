9d400167a323fb89be78a257d4104ae41c040b1c
coolestProcaviidae <85066178+genericWindowsUser416@users.noreply.github.com>
2024-10-12 12:21:40 +0300

    replaced Assert.AreEqual(expected, actual); with Assert.That(actual, Is.EqualTo(expected));

--- homework05Test/homework01Tests.cs
+++ homework05Test/homework01Tests.cs
@@ -9,21 +9,21 @@ namespace homework05Test
         public void getDeliveryCostTest(string userInput, double expected)
         {
             double actual = homework01.getDeliveryCost(userInput);
-            Assert.AreEqual(expected, actual);
+            Assert.That(actual, Is.EqualTo(expected));
         }
 
         [TestCase("200", "100", 10)]
         public void getBonusAmountTest(string userInputTotalSells, string userInputPlannedSells, double expected)
         {
             double actual = homework01.getBonusAmount(userInputTotalSells, userInputPlannedSells);
-            Assert.AreEqual(expected, actual);
+            Assert.That(actual, Is.EqualTo(expected));
         }
 
         [TestCase("100", 115)]
         public void getInvestmentEarnAmountTest(string userInputDepositAmount, double expected)
         {
             double actual = homework01.getInvestmentEarnAmount(userInputDepositAmount);
-            Assert.AreEqual(expected, actual);
+            Assert.That(actual, Is.EqualTo(expected));
         }
     }
 }
